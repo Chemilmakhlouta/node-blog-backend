4a2a389185634b93707827b666fe2ebd0be2adaa
Chemil <chemil.makhlouta@gmail.com>
2020-07-26 23:57:54 -0700

    Adds SQL scripts
    Runs migration
    Creates SQL to build tables

--- database/actions/run-migration.js
+++ database/actions/run-migration.js
@@ -9,14 +9,33 @@ function replaceAll(str, delimeter, replacement) {
 }
 
 
-// fs.readdir(
-//     path.resolve(
-//         __dirName,
-//         '../migrations'
-//     ),
-//     "utf-8",
-//     (err, files) => {
-//         if(err) throw new Error(err)
-//         return promise.each(files)
-//     }
-// )
+fs.readdir(
+    path.resolve(
+        __dirName,
+        '../migrations'
+    ),
+    "utf-8",
+    (err, files) => {
+        if(err) throw new Error(err)
+        return promise.each(files, (fileName) => {
+            return new Promise((resolve,reject) => {
+                fs.readFile(
+                    path.resolve(
+                        __dirname,
+                        '../migrations/${fileName}'
+                    ),
+                    'utf-8',
+                    (err,sql) => {
+                        if (err) reject(err)
+                        return knex.raw(replaceAll(sql, '{}', db))
+                        .then(resolve)
+                    }
+                )
+            })
+        })
+        .then(() => console.log("Migrations have run successfully"))
+        .catch(err => {
+            throw new Error(err)
+        })
+    }
+)
